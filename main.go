53946c2cc758bb78d53f54a0fadc6510b2db5da2
Vase Ko <vasev85@gmail.com>
2022-06-30 01:03:47 +0300

    main go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func hello(name string) string {
+	return fmt.Sprintf("Hello %s!", name)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("Missing name: hello <name>")
+	}
+	fmt.Println(hello(os.Args[1]))
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+
